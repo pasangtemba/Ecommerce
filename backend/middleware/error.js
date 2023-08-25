5d702e289a48291a0ee3e4d1d0a21664662d6cf8
pasangtemba <pasangtemba.sherpa5@gmail.com>
2023-08-25 23:59:42 +0545

    Added Error Handler

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -2,6 +2,13 @@ const ErrorHandler = require("../utils/errorhandler");
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
+
+  //Wrong mongoDB ID error
+  if (err.name === caseError) {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ const path = require("path");
 const { Server } = require("http");
 const connectDatabase = require("./config/database");
 
+//handeling uncought exception
+process.on(`uncaughtException`, (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log(`Shutting down the server due to uncought exception`);
+  process.exit(1);
+});
+
 //config
 
 dotenv.config({ path: "backend/config/config.env" });
@@ -14,4 +21,14 @@ dotenv.config({ path: "backend/config/config.env" });
 connectDatabase();
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://loclhost: ${process.env.PORT}`);
+});
+
+//handeled promise Rejection
+
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("Shutting down the server due to Unhandled Promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
