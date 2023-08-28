f48c23065658ed46cdb0ae6aa2750791b95dc373
pasangtemba <pasangtemba.sherpa5@gmail.com>
2023-08-29 00:44:03 +0545

    user login

--- backend/contollers/userController.js
+++ backend/contollers/userController.js
@@ -1,22 +1,46 @@
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncError = require("../middleware/catchAcyncError");
-const user = require("../models/userModel");
+const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
 
 //Registration of user
 
 exports.registerUser = catchAsyncError(async (req, res, next) => {
-  const [name, email, password] = req.body;
-  const user = await user.create({
+  const { name, email, password } = req.body;
+  const user = await User.create({
     name,
     email,
     password,
     avatar: {
       public_id: "this is a sample id",
-      url: "profilepic.jpg",
+      url: "profilepic",
     },
   });
-  res.status(201).json({
-    success: true,
-    user,
-  });
+
+  sendToken(user, 201, res);
+});
+
+//Login User
+exports.loginUser = catchAsyncError(async function loginUser(req, res, next) {
+  const { email, password } = req.body;
+
+  //Check if email and password is entered by user
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+
+  //Check if password is correct or not
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+
+  sendToken(user, 200, res);
 });
