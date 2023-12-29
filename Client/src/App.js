01c5ea9dd23ee92c081523f900872a4f12c12403
pasangtemba <pasangtemba.sherpa5@gmail.com>
2023-12-29 21:05:44 +0545

    comit

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Headers from "./components/layout/Header/Headers.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import WebFont from "webfontloader";
 
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "sans-serif"],
+      },
+    });
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Headers />
+    </Router>
   );
 }
 
